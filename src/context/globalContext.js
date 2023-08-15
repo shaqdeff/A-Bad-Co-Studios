a284963b5586aec88faed8f8c9685524b9380e8c
shaqdeff <shaquillendunda@gmail.com>
2023-08-16 02:53:29 +0300

    Define context & create hooks for dispact & state

---
+++ src/context/globalContext.js
@@ -0,0 +1,38 @@
+import React, { createContext, useReducer, useContext } from "react"
+
+// define the context
+const GlobalStateContext = createContext()
+const GlobalDispatchContext = createContext()
+
+// reducer
+const globalReducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_THEME": {
+      return {
+        ...state,
+        currentTheme: action.theme,
+      }
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
+}
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(globalReducer, {
+    currentTheme: "dark",
+  })
+
+  return (
+    <GlobalDispatchContext.Provider value={dispatch}>
+      <GlobalStateContext.Provider value={state}>
+        {children}
+      </GlobalStateContext.Provider>
+    </GlobalDispatchContext.Provider>
+  )
+}
+
+// custom hooks to use dispatch and state
+export const useGlobalStateContext = () => useContext(GlobalStateContext)
+export const useGlobalDispatchContext = () => useContext(GlobalDispatchContext)
