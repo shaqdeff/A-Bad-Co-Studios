bef78ce394e33d8b3aaadf5023dd5bed6fa50bda
shaqdeff <shaquillendunda@gmail.com>
2023-08-16 02:56:37 +0300

    Import switch component and context

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,56 @@
 import React from "react"
 import { Link } from "gatsby"
 
+// components
+import Switch from "./switch"
+
 // styled components
 import { Container, Flex, Logo, Menu, HeaderNav } from "../styles"
 
 // images
 import { lens } from "../images"
 
+// context
+import {
+  useGlobalStateContext,
+  useGlobalDispatchContext,
+} from "../context/globalContext"
+
 const Header = () => {
+  const dispatch = useGlobalDispatchContext()
+  const { currentTheme } = useGlobalStateContext()
+
+  const toggleTheme = () => {
+    if (currentTheme === "light") {
+      dispatch({ type: "TOGGLE_THEME", theme: "dark" })
+    } else {
+      dispatch({ type: "TOGGLE_THEME", theme: "light" })
+    }
+  }
+
   return (
     <HeaderNav>
       <Container>
         <Flex spaceBetween noHeight>
-          <Logo>
-            <Link to="/">A BAD C</Link>
-            <span>
-              <img src={lens} alt="lens" className="lens" />
-            </span>
-          </Logo>
+          <div className="logo-container">
+            <Logo>
+              <Link to="/">A BAD C</Link>
+              <span>
+                <Link to="/">
+                  <img
+                    src={lens}
+                    alt="lens"
+                    className="lens"
+                    // onClick={toggleTheme}
+                  />
+                </Link>
+              </span>
+            </Logo>
+
+            <Link to="/" className="studios">
+              STUDIOS
+            </Link>
+          </div>
 
           <Menu>
             <button>
@@ -26,6 +59,10 @@ const Header = () => {
             </button>
           </Menu>
         </Flex>
+
+        <div className="switch-box">
+          <Switch />
+        </div>
       </Container>
     </HeaderNav>
   )
