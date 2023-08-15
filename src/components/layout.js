8f56affc6c84ce21dbbeed4a33db6f1e25334cc8
shaqdeff <shaquillendunda@gmail.com>
2023-08-15 16:42:47 +0300

    Add reset styles and theme provider

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,33 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+// styled components
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { normalize } from "styled-normalize"
+
+const GlobalStyle = createGlobalStyle`
+${normalize}
+
+* {
+  text-decoration: none;
+  /* cursor: none; */
+}
+
+html {
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  font-size: 16px;
+}
+
+body {
+  font-family:  -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  background: ${props => props.theme.background};
+  overscroll-behavior: none;
+  overflow-x: hidden;
+}
+
+`
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -13,7 +40,22 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return <main>{children}</main>
+  const darkTheme = {
+    background: "#141414",
+    text: "#fdfbed",
+  }
+
+  const lightTheme = {
+    background: "#fdfbed",
+    text: "#1f1f1f",
+  }
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <main>{children}</main>
+    </ThemeProvider>
+  )
 }
 
 Layout.propTypes = {
