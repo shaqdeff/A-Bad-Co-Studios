f4170fb859ec0f6c99a2393ab89b778f23759a4b
shaqdeff <shaquillendunda@gmail.com>
2023-08-15 20:33:01 +0300

    Add logo and menu button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,34 @@
 import React from "react"
+import { Link } from "gatsby"
+
+// styled components
+import { Container, Flex, Logo, Menu, HeaderNav } from "../styles"
+
+// images
+import { lens } from "../images"
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <HeaderNav>
+      <Container>
+        <Flex spaceBetween noHeight>
+          <Logo>
+            <Link to="/">A BAD C</Link>
+            <span>
+              <img src={lens} alt="lens" className="lens" />
+            </span>
+          </Logo>
+
+          <Menu>
+            <button>
+              <span></span>
+              <span></span>
+            </button>
+          </Menu>
+        </Flex>
+      </Container>
+    </HeaderNav>
+  )
 }
 
 export default Header
