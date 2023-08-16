412a390bd78ddab94359142ea8e3f387bc5cc01e
shaqdeff <shaquillendunda@gmail.com>
2023-08-16 03:31:13 +0300

    Move styled component to headerStyles.js

--- src/components/switch.jsx
+++ src/components/switch.jsx
@@ -1,8 +1,4 @@
 import React, { useState } from "react"
-import styled from "styled-components"
-
-// icons
-import { MoonIcon, SunIcon } from "../assets"
 
 // context
 import {
@@ -10,53 +6,11 @@ import {
   useGlobalDispatchContext,
 } from "../context/globalContext"
 
-const SwitchStyles = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 5px;
+// styled components
+import { ToggleSwitch } from "../styles"
 
-  .toggle-switch {
-    position: relative;
-    display: inline-block;
-    width: 18px;
-    height: 40px;
-    margin: 0 0.75rem;
-  }
-  .toggle-switch input[type="checkbox"] {
-    display: none;
-  }
-  .toggle-switch .switch {
-    position: absolute;
-    cursor: pointer;
-    background-color: #fbfbed;
-    border-radius: 25px;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    transition: background-color 0.2s ease;
-  }
-  .toggle-switch .switch::before {
-    position: absolute;
-    content: "";
-    left: 3px;
-    top: 7px;
-    width: 12px;
-    height: 12px;
-    background-color: #1f1f1f;
-    border-radius: 50%;
-    transition: transform 0.3s ease;
-  }
-  .toggle-switch input[type="checkbox"]:checked + .switch::before {
-    transform: translateY(15px);
-    background-color: #fbfbed;
-  }
-  .toggle-switch input[type="checkbox"]:checked + .switch {
-    background-color: #1f1f1f;
-  }
-`
+// icons
+import { MoonIcon, SunIcon } from "../assets"
 
 function Switch() {
   const dispatch = useGlobalDispatchContext()
@@ -74,16 +28,14 @@ function Switch() {
   }
 
   return (
-    <>
-      <SwitchStyles>
-        <SunIcon />
-        <label className="toggle-switch">
-          <input type="checkbox" checked={isToggled} onChange={onToggle} />
-          <span className="switch" />
-        </label>
-        <MoonIcon />
-      </SwitchStyles>
-    </>
+    <ToggleSwitch>
+      <SunIcon />
+      <label className="toggle-switch">
+        <input type="checkbox" checked={isToggled} onChange={onToggle} />
+        <span className="switch" />
+      </label>
+      <MoonIcon />
+    </ToggleSwitch>
   )
 }
 export default Switch
