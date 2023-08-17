ef845c4217b3f311057a9a1c4a01fbad057f370c
shaqdeff <shaquillendunda@gmail.com>
2023-08-17 21:56:53 +0300

    Import home styles

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,20 @@
-import { HeaderNav, Logo, Menu, ToggleSwitch } from "./headerStyles"
 import { Container } from "./globalStyles"
 import { Cursor } from "./globalStyles"
 import { Flex } from "./globalStyles"
+import { HeaderNav, Logo, Menu, ToggleSwitch } from "./headerStyles"
+import { Banner, Video, Canvas, BannerTitle, Headline } from "./homeStyles"
 
-export { Container, Cursor, Flex, HeaderNav, Logo, Menu, ToggleSwitch }
+export {
+  Container,
+  Cursor,
+  Flex,
+  HeaderNav,
+  Logo,
+  Menu,
+  ToggleSwitch,
+  Banner,
+  Video,
+  Canvas,
+  BannerTitle,
+  Headline,
+}
