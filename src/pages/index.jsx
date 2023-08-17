f0c0faee74076ec58a05a7610c4eea2556978980
shaqdeff <shaquillendunda@gmail.com>
2023-08-18 01:48:02 +0300

    Import homeBanner.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,12 @@
 import React from "react"
-import Layout from "../components/layout"
+import {Layout, HomeBanner} from "../components"
 
 const IndexPage = props => {
-  return <Layout>Wassup!</Layout>
+  return (
+    <Layout>
+      <HomeBanner />
+    </Layout>
+  )
 }
 
 export default IndexPage
