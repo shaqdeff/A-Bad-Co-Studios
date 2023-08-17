08e7ffc02aba3a7567dc1ac522052b14a5cb547d
shaqdeff <shaquillendunda@gmail.com>
2023-08-17 21:57:09 +0300

    Import home banner

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import CustomCursor from "./customCursor"
 import Header from "./header"
+import HomeBanner from "./home/homeBanner"
 import Layout from "./layout"
 import SEO from "./seo"
 import Switch from "./switch"
 
-export { CustomCursor, Header, Layout, SEO, Switch }
+export { CustomCursor, Header, HomeBanner, Layout, SEO, Switch }
