3a4a7ef0b81c288f1e531574dcea90b14ed85fdc
shaqdeff <shaquillendunda@gmail.com>
2023-08-17 19:22:32 +0300

    Import new video assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,26 @@
+// icons
 import MoonIcon from "./svg/MoonIcon"
 import SunIcon from "./svg/SunIcon"
 
-export { MoonIcon, SunIcon }
+// videos
+import beginnings from "./video/beginnings.mp4"
+import capture from "./video/capture-it.mp4"
+import cruiser from "./video/cruiser.mp4"
+import colorist from "./video/colorist.mp4"
+import featured from "./video/featured-video.mp4"
+import lights from "./video/lights-on.mp4"
+import next from "./video/next.mp4"
+import wild from "./video/wild-dreams.mp4"
+
+export {
+  MoonIcon,
+  SunIcon,
+  beginnings,
+  capture,
+  cruiser,
+  colorist,
+  featured,
+  lights,
+  next,
+  wild,
+}
