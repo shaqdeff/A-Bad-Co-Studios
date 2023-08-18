51b4a6c5e1bb885ee785cf2a77085f48b4f91aa5
shaqdeff <shaquillendunda@gmail.com>
2023-08-18 03:51:28 +0300

    Create onCursor function

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,15 @@
 import React from "react"
-import {Layout, HomeBanner} from "../components"
+import { Layout, HomeBanner } from "../components"
 
 const IndexPage = props => {
+  const onCursor = cursorType => {
+    cursorType = (cursorStyles.includes(cursorType) && cursorType) || false
+    dispatch({ type: "CURSOR_TYPE", cursorType: cursorType })
+  }
+
   return (
     <Layout>
-      <HomeBanner />
+      <HomeBanner onCursor={onCursor} />
     </Layout>
   )
 }
