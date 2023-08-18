2854aef8f28edafaf06b0090e9f31744f9316282
shaqdeff <shaquillendunda@gmail.com>
2023-08-18 04:02:55 +0300

    Import global state and dispatch context

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,15 @@
 import React from "react"
+
+// components
 import { Layout, HomeBanner } from "../components"
 
+// context
+import { useGlobalStateContext, useGlobalDispatchContext } from "../context"
+
 const IndexPage = props => {
+  const { currentTheme, cursorStyles } = useGlobalStateContext()
+  const dispatch = useGlobalDispatchContext()
+
   const onCursor = cursorType => {
     cursorType = (cursorStyles.includes(cursorType) && cursorType) || false
     dispatch({ type: "CURSOR_TYPE", cursorType: cursorType })
