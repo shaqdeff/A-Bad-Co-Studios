5fc24a8acdc7cc332f11ac45a317851d3cd7f313
shaqdeff <shaquillendunda@gmail.com>
2023-08-21 00:00:48 +0300

    Add navRoutes data

---
+++ src/data/index.js
@@ -0,0 +1,3 @@
+import { navRoutes } from "./navRoutes"
+
+export { navRoutes }
---
+++ src/data/navRoutes.js
@@ -0,0 +1,12 @@
+export const navRoutes = [
+  { id: 0, title: "beginnings", path: "/beginnings", video: "beginnings.mp4" },
+  { id: 1, title: "what's next?", path: "/whats-next", video: "next.mp4" },
+  { id: 2, title: "lights on", path: "/lights-on", video: "lights-on.mp4" },
+  { id: 3, title: "colorist", path: "/colorist", video: "colorist.mp4" },
+  {
+    id: 4,
+    title: "wild dreams",
+    path: "/wild-dreams",
+    video: "wild-dreams.mp4",
+  },
+]
