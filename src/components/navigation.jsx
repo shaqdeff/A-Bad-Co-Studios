caf918cba463d2e0f4ac7f63fc0db72549e39d89
shaqdeff <shaquillendunda@gmail.com>
2023-08-21 00:40:37 +0300

    Add nav routes to nav list

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,7 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
-// styled component
-import { Container, Flex } from "../styles"
+// styled components
+import {
+  Container,
+  Flex,
+  Nav,
+  NavHeader,
+  CloseNav,
+  NavList,
+  NavFooter,
+} from "../styles"
+
+// data
+import { navRoutes } from "../data"
 
 const Navigation = () => {
   return (
@@ -21,8 +33,30 @@ const Navigation = () => {
 
         <NavList>
           <ul>
+            {navRoutes.map(route => (
+              <li key={route.id}>
+                <Link to={`/projects/${route.path}`}>
+                  <div className="link">
+                    <span className="arrow">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 101 57"
+                      >
+                        <path
+                          d="M33 34H0V24h81.429L66 7.884 73.548 0l19.877 20.763.027-.029L101 28.618 73.829 57l-7.548-7.884L80.753 34H33z"
+                          fill="#FFF"
+                          fillRule="evenodd"
+                        ></path>
+                      </svg>
+                      {route.title}
+                    </span>
+                  </div>
+                </Link>
+              </li>
+            ))}
+
             <li>
-              <Link to="/projects/project-name">
+              <Link to="/projects/gallery">
                 <div className="link">
                   <span className="arrow">
                     <svg
@@ -35,7 +69,7 @@ const Navigation = () => {
                         fillRule="evenodd"
                       ></path>
                     </svg>
-                    BEGINNINGS
+                    gallery
                   </span>
                 </div>
               </Link>
