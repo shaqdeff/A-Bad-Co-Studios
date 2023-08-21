7ec0a9f6f4d9954e2cebdf3b68b260d730cda046
shaqdeff <shaquillendunda@gmail.com>
2023-08-21 16:57:44 +0300

    Change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/logo/berry-lens.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo/lens.png`, // This path is relative to the root of the site.
       },
     },
   ],
