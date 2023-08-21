2de6e58af842b45641b05267c92e7e1ed17e2084
shaqdeff <shaquillendunda@gmail.com>
2023-08-21 03:36:56 +0300

    Add toggleMenu and setToggleMenu as props

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -20,7 +20,7 @@ import { navRoutes } from "../data"
 // assets
 import { beginnings, next, lights, colorist, wild } from "../assets"
 
-const Navigation = () => {
+const Navigation = ({ toggleMenu, setToggleMenu }) => {
   const [revealVideo, setRevealVideo] = useState({
     show: false,
     video: beginnings,
@@ -28,131 +28,137 @@ const Navigation = () => {
   })
 
   return (
-    <Nav>
-      <Container>
-        <NavHeader>
-          <Flex spacebetween="true" noheight="true">
-            <h2>Projects</h2>
-            <CloseNav>
-              <button>
-                <span></span>
-                <span></span>
-              </button>
-            </CloseNav>
-          </Flex>
-        </NavHeader>
+    <>
+      <AnimatePresence>
+        {toggleMenu && (
+          <Nav>
+            <Container>
+              <NavHeader>
+                <Flex spacebetween="true" noheight="true">
+                  <h2>Projects</h2>
+                  <CloseNav>
+                    <button>
+                      <span></span>
+                      <span></span>
+                    </button>
+                  </CloseNav>
+                </Flex>
+              </NavHeader>
 
-        <NavList>
-          <ul>
-            {navRoutes.map(route => (
-              <motion.li
-                key={route.id}
-                onHoverStart={() =>
-                  setRevealVideo({
-                    show: true,
-                    video: route.video,
-                    key: route.id,
-                  })
-                }
-                onHoverEnd={() =>
-                  setRevealVideo({
-                    show: false,
-                    video: route.video,
-                    key: route.id,
-                  })
-                }
-              >
-                <Link to={`/projects${route.path}`}>
-                  <motion.div
-                    initial={{ x: -108 }}
-                    whileHover={{
-                      x: -40,
-                      transition: {
-                        duration: 0.4,
-                        ease: [0.6, 0.05, 0.01, 0.9],
-                      },
-                    }}
-                    className="link"
-                  >
-                    <span className="arrow">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 101 57"
-                      >
-                        <path
-                          d="M33 34H0V24h81.429L66 7.884 73.548 0l19.877 20.763.027-.029L101 28.618 73.829 57l-7.548-7.884L80.753 34H33z"
-                          fill="#FFF"
-                          fillRule="evenodd"
-                        ></path>
-                      </svg>
-                      {route.title}
-                    </span>
-                  </motion.div>
-                </Link>
-              </motion.li>
-            ))}
-
-            <li>
-              <Link to="/">
-                <motion.div
-                  initial={{ x: -108 }}
-                  whileHover={{
-                    x: -40,
-                    transition: {
-                      duration: 0.4,
-                      ease: [0.6, 0.05, 0.01, 0.9],
-                    },
-                  }}
-                  className="link"
-                >
-                  <span className="arrow">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 101 57"
+              <NavList>
+                <ul>
+                  {navRoutes.map(route => (
+                    <motion.li
+                      key={route.id}
+                      onHoverStart={() =>
+                        setRevealVideo({
+                          show: true,
+                          video: route.video,
+                          key: route.id,
+                        })
+                      }
+                      onHoverEnd={() =>
+                        setRevealVideo({
+                          show: false,
+                          video: route.video,
+                          key: route.id,
+                        })
+                      }
                     >
-                      <path
-                        d="M33 34H0V24h81.429L66 7.884 73.548 0l19.877 20.763.027-.029L101 28.618 73.829 57l-7.548-7.884L80.753 34H33z"
-                        fill="#FFF"
-                        fillRule="evenodd"
-                      ></path>
-                    </svg>
-                    gallery
-                  </span>
-                </motion.div>
-              </Link>
-            </li>
-          </ul>
-        </NavList>
+                      <Link to={`/projects${route.path}`}>
+                        <motion.div
+                          initial={{ x: -108 }}
+                          whileHover={{
+                            x: -40,
+                            transition: {
+                              duration: 0.4,
+                              ease: [0.6, 0.05, 0.01, 0.9],
+                            },
+                          }}
+                          className="link"
+                        >
+                          <span className="arrow">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 101 57"
+                            >
+                              <path
+                                d="M33 34H0V24h81.429L66 7.884 73.548 0l19.877 20.763.027-.029L101 28.618 73.829 57l-7.548-7.884L80.753 34H33z"
+                                fill="#FFF"
+                                fillRule="evenodd"
+                              ></path>
+                            </svg>
+                            {route.title}
+                          </span>
+                        </motion.div>
+                      </Link>
+                    </motion.li>
+                  ))}
 
-        <NavFooter></NavFooter>
+                  <li>
+                    <Link to="/">
+                      <motion.div
+                        initial={{ x: -108 }}
+                        whileHover={{
+                          x: -40,
+                          transition: {
+                            duration: 0.4,
+                            ease: [0.6, 0.05, 0.01, 0.9],
+                          },
+                        }}
+                        className="link"
+                      >
+                        <span className="arrow">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 101 57"
+                          >
+                            <path
+                              d="M33 34H0V24h81.429L66 7.884 73.548 0l19.877 20.763.027-.029L101 28.618 73.829 57l-7.548-7.884L80.753 34H33z"
+                              fill="#FFF"
+                              fillRule="evenodd"
+                            ></path>
+                          </svg>
+                          gallery
+                        </span>
+                      </motion.div>
+                    </Link>
+                  </li>
+                </ul>
+              </NavList>
 
-        <NavVideos>
-          <motion.div
-            animate={{ width: revealVideo.show ? 0 : "100%" }}
-            className="reveal"
-          ></motion.div>
+              <NavFooter></NavFooter>
 
-          <div className="video">
-            <AnimatePresence initial={false} mode="wait">
-              <motion.video
-                key={revealVideo.key}
-                src={revealVideo.video}
-                initial={{ opacity: 0 }}
-                exit={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{
-                  duration: 0.2,
-                  ease: "easeInOut",
-                }}
-                autoPlay
-                muted
-                loop
-              ></motion.video>
-            </AnimatePresence>
-          </div>
-        </NavVideos>
-      </Container>
-    </Nav>
+              <NavVideos>
+                <motion.div
+                  animate={{ width: revealVideo.show ? 0 : "100%" }}
+                  className="reveal"
+                ></motion.div>
+
+                <div className="video">
+                  <AnimatePresence initial={false} mode="wait">
+                    <motion.video
+                      key={revealVideo.key}
+                      src={revealVideo.video}
+                      initial={{ opacity: 0 }}
+                      exit={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        ease: "easeInOut",
+                      }}
+                      autoPlay
+                      muted
+                      loop
+                    ></motion.video>
+                  </AnimatePresence>
+                </div>
+              </NavVideos>
+            </Container>
+          </Nav>
+        )}
+      </AnimatePresence>
+    </>
   )
 }
 
