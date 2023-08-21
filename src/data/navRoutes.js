7656e1af728765992e718afb61d82d62cebf4e62
shaqdeff <shaquillendunda@gmail.com>
2023-08-21 03:17:55 +0300

    Update nav routes

--- src/data/navRoutes.js
+++ src/data/navRoutes.js
@@ -1,12 +1,35 @@
+// assets
+import { beginnings, next, lights, colorist, wild } from "../assets"
+
 export const navRoutes = [
-  { id: 0, title: "beginnings", path: "/beginnings", video: "beginnings.mp4" },
-  { id: 1, title: "what's next?", path: "/whats-next", video: "next.mp4" },
-  { id: 2, title: "lights on", path: "/lights-on", video: "lights-on.mp4" },
-  { id: 3, title: "colorist", path: "/colorist", video: "colorist.mp4" },
+  {
+    id: 0,
+    title: "beginnings",
+    path: "/beginnings",
+    video: beginnings,
+  },
+  {
+    id: 1,
+    title: "what's next?",
+    path: "/whats-next",
+    video: next,
+  },
+  {
+    id: 2,
+    title: "lights on",
+    path: "/lights-on",
+    video: lights,
+  },
+  {
+    id: 3,
+    title: "colorist",
+    path: "/colorist",
+    video: colorist,
+  },
   {
     id: 4,
     title: "wild dreams",
     path: "/wild-dreams",
-    video: "wild-dreams.mp4",
+    video: wild,
   },
 ]
