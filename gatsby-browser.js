4d8aef97ee664a4e7c163de37f47aec84aa5e4a3
shaqdeff <shaquillendunda@gmail.com>
2023-08-28 04:15:46 +0300

    Write function to log client route changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,11 @@ import React from "react"
 
 import { GlobalProvider } from "./src/context/globalContext"
 
+export const onRouteUpdate = ({ location, prevLocation }) => {
+  console.log("new pathname", location.pathname)
+  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
+}
+
 export const wrapRootElement = ({ element }) => (
   <GlobalProvider>{element}</GlobalProvider>
 )
