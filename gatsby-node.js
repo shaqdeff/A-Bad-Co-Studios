7b8f4757826ff08cca78be5284588a1fc88166e6
shaqdeff <shaquillendunda@gmail.com>
2023-08-28 04:36:08 +0300

    Add function to dynamically create pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,24 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
+const path = require("path")
+const pageData = require("./src/data/images.json")
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+  // createPage({
+  //   path: "/using-dsg",
+  //   component: require.resolve("./src/templates/using-dsg.js"),
+  //   context: {},
+  //   defer: true,
+  // })
+
+  pageData.forEach(data => {
+    const pagePath = `/${data.slug}`:
+      
+    createPage({
+      path: pagePath,
+      component: path.resolve("./src/components/gallery/index.jsx"),
+      context: data
+    })
   })
 }
