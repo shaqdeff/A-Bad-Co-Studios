eaf74abb4a7455c363fd02a9c76c94d55b932b98
shaqdeff <shaquillendunda@gmail.com>
2023-08-29 05:20:06 +0300

    Dynamically create page for gallery data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,6 @@
+const path = require("path")
+const pageData = require("./src/data/images.js")
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
@@ -5,5 +8,15 @@ exports.createPages = async ({ actions }) => {
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+
+  pageData.forEeach(data => {
+    const pagePath = `/${data.slug}`
+
+    createPage({
+      path: pagePath,
+      component: path.resolve(`./src/components/model/index.tsx`),
+      context: data,
+    })
   })
 }
