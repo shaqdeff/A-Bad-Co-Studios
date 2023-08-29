46748e1dc0e6dbaa2a4d34bbcaecfb09a5ff6f2b
shaqdeff <shaquillendunda@gmail.com>
2023-08-29 06:23:48 +0300

    Add data to images.js

--- src/data/images.js
+++ src/data/images.js
@@ -1,8 +1,216 @@
 export const images = [
+  // fashion
   {
-    cover: "",
-    title: "",
-    slug: "",
+    cover:
+      "https://images.unsplash.com/photo-1527203561188-dae1bc1a417f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1015&q=80",
+    title: "Goddess",
+    slug: "goddess",
     color: "",
   },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1530884698386-d42ad3199b1f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "Vintage Mars",
+    slug: "vintage-mars",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1542513217-0b0eedf7005d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
+    title: "Melanin Queen",
+    slug: "melanin-queen",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1535530705774-695729778c55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "Tru Identity",
+    slug: "tru-identity",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1573162201267-83bddd2a5f0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "Iconic",
+    slug: "iconic",
+    color: "",
+  },
+
+  // culture
+  {
+    cover:
+      "https://images.unsplash.com/photo-1515658323406-25d61c141a6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80",
+    title: "Nomadic Bliss",
+    slug: "nomadic-bliss",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1603486037214-4fec4016a9bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1227&q=80",
+    title: "The Hmong Girl",
+    slug: "the-hmong-girl",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1610648926872-37fe9b90a487?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=988&q=80",
+    title: "Carnivalous",
+    slug: "carnivalous",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1542897643-cfccd88c7127?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1227&q=80",
+    title: "Penglipuran",
+    slug: "penglipuran",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1593489062665-9f26fa627d73?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "Mehendi",
+    slug: "mehendi",
+    color: "",
+  },
+
+  // architecture
+  {
+    cover:
+      "https://images.unsplash.com/photo-1558594144-7eeeaa34d22b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1036&q=80",
+    title: "Capitoline",
+    slug: "capitoline",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1665340825807-431992a97de3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "Tan Teng Niah",
+    slug: "tan-teng-niah",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1596446391793-b0f08023114e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "Taipei",
+    slug: "taipei",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1564898148101-82f74d4df7f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1035&q=80",
+    title: "Hut",
+    slug: "hut",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1534943441045-1009d7cb0bb9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1016&q=80",
+    title: "Cartagena",
+    slug: "cartagena",
+    color: "",
+  },
+
+  // food
+  {
+    cover:
+      "https://images.unsplash.com/photo-1563805042-7684c019e1cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1227&q=80",
+    title: "Oreo Dessert",
+    slug: "oreo-dessert",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "Bbq Kebabs",
+    slug: "bbq-kebabs",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1550989460-0adf9ea622e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "Vegatables",
+    slug: "vegetables",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1574164052277-b9ea797de8e4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "Dumplings",
+    slug: "dumplings",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1559737558-2f5a35f4523b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "Shrimps",
+    slug: "shrimps",
+    color: "",
+  },
+
+  // cities
+  {
+    cover:
+      "https://images.unsplash.com/photo-1554797589-7241bb691973?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1036&q=80",
+    title: "Tokyo",
+    slug: "tokyo",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1611144727915-ef30a08aaeb3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=986&q=80",
+    title: "Nairobi",
+    slug: "nairobi",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1613390250147-171878866f04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1036&q=80",
+    title: "Rio De Janeiro",
+    slug: "rio-de-janeiro",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1541336032412-2048a678540d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    title: "New York",
+    slug: "new-york",
+    color: "",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1566552881560-0be862a7c445?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1035&q=80",
+    title: "Mumbai",
+    slug: "mumbai",
+    color: "",
+  },
+  // {
+  //   cover: "",
+  //   title: "",
+  //   slug: "",
+  //   color: "",
+  // },
+  // {
+  //   cover: "",
+  //   title: "",
+  //   slug: "",
+  //   color: "",
+  // },
+  // {
+  //   cover: "",
+  //   title: "",
+  //   slug: "",
+  //   color: "",
+  // },
+  // {
+  //   cover: "",
+  //   title: "",
+  //   slug: "",
+  //   color: "",
+  // },
+  // {
+  //   cover: "",
+  //   title: "",
+  //   slug: "",
+  //   color: "",
+  // },
 ]
