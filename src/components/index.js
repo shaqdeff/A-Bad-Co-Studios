128313bc80cc6374cfb3000d785e73eafb6a2e57
shaqdeff <shaquillendunda@gmail.com>
2023-08-29 22:22:49 +0300

    Import and export gallery content and gallery header

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import CustomCursor from "./customCursor"
 import Header from "./header"
 import HomeBanner from "./home/homeBanner"
+import GalleryContent from "./gallery/galleryContent"
+import GalleryHeader from "./gallery/galleryHeader"
 import GalleryLayout from "./gallery/galleryLayout"
 import Layout from "./layout"
 import Navigation from "./navigation"
@@ -11,6 +13,8 @@ export {
   CustomCursor,
   Header,
   HomeBanner,
+  GalleryContent,
+  GalleryHeader,
   GalleryLayout,
   Layout,
   Navigation,
