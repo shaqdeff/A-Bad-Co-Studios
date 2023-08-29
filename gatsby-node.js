c5fd315c0c736b63a05e1a3845ee9a1a7bc5dfb6
shaqdeff <shaquillendunda@gmail.com>
2023-08-29 04:33:18 +0300

    Update gatsby-node.js file

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,9 @@
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
-  // createPage({
-  //   path: "/using-dsg",
-  //   component: require.resolve("./src/templates/using-dsg.js"),
-  //   context: {},
-  //   defer: true,
-  // })
-
-  pageData.forEach(data => {
-    const pagePath = `/${data.slug}`:
-      
-    createPage({
-      path: pagePath,
-      component: path.resolve("./src/components/gallery/index.jsx"),
-      context: data
-    })
+  createPage({
+    path: "/using-dsg",
+    component: require.resolve("./src/templates/using-dsg.js"),
+    context: {},
+    defer: true,
   })
 }
