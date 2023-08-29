5ed39a092ca3fb766ebd9b91948f8dd0b34131ee
shaqdeff <shaquillendunda@gmail.com>
2023-08-29 07:26:16 +0300

    Map image data

--- src/components/gallery/galleryContent.tsx
+++ src/components/gallery/galleryContent.tsx
@@ -1,7 +1,61 @@
-import React from "react"
+import React, { useState } from "react"
+import { images } from "../../data"
+
+// components
+import ImageLink from "./imageLink"
+
+// styled components
+import {
+  Content,
+  GridContent,
+  ListContent,
+  GridElements,
+  GridElement,
+  ThumbnailWrapper,
+} from "../../styles"
+
+export type DataType = {
+  cover: string
+  title: string
+  slug: string
+  color: string
+}
 
 const GalleryContent = () => {
-  return <div>GalleryContent</div>
+  const [gridVisible, setGridVisible] = useState(true)
+  const mapData: DataType[] = Array.from(images)
+
+  return (
+    <>
+      <Content>
+        {gridVisible && (
+          <GridContent>
+            <GridElements>
+              {mapData.map((element, index) => (
+                <GridElement>
+                  <ThumbnailWrapper>
+                    <ImageLink element={element} index={index} />
+                  </ThumbnailWrapper>
+                </GridElement>
+              ))}
+            </GridElements>
+          </GridContent>
+        )}
+
+        {!gridVisible && (
+          <ListContent>
+            {mapData.map((element, index) => (
+              <GridElement>
+                <ThumbnailWrapper>
+                  <ImageLink element={element} index={index} />
+                </ThumbnailWrapper>
+              </GridElement>
+            ))}
+          </ListContent>
+        )}
+      </Content>
+    </>
+  )
 }
 
 export default GalleryContent
