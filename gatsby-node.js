0077632e1a4539fdc06a81a6512327f0a38251d5
shaqdeff <shaquillendunda@gmail.com>
2023-08-30 02:44:15 +0300

    Modify createPage function

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,20 +3,16 @@ const pageData = require("./src/data/images.js")
 
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
 
-  pageData.forEeach(data => {
-    const pagePath = `/${data.slug}`
+  pageData.forEach(data => {
+    const pagePath = `/projects/gallery/${data.slug}`
 
     createPage({
       path: pagePath,
-      component: path.resolve(`./src/components/model/index.tsx`),
-      context: data,
+      component: path.resolve("./src/templates/imagePage.tsx"),
+      context: {
+        slug: data.slug,
+      },
     })
   })
 }
