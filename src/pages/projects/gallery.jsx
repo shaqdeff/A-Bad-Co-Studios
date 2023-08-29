5c056f00f5559c5ee1ba1f13c5343db77883c3d2
shaqdeff <shaquillendunda@gmail.com>
2023-08-29 05:38:39 +0300

    Import galleryContent.tsx

--- src/pages/projects/gallery.jsx
+++ src/pages/projects/gallery.jsx
@@ -1,9 +1,20 @@
 import React from "react"
+import Loadable from "react-loadable"
+
+function LoadingComponent(props) {
+  return <div />
+}
+
+// lazy load of component
+const GalleryLazy = Loadable({
+  loader: () => import("../../components/gallery/galleryContent"),
+  loading: LoadingComponent,
+})
 
 const Gallery = () => {
   return (
     <>
-      <div>Gallery</div>
+      <GalleryLazy />
     </>
   )
 }
