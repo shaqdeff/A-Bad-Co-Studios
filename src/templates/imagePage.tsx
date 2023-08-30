559811757578b20368057f1d183cabbd41fc358a
shaqdeff <shaquillendunda@gmail.com>
2023-08-30 04:55:57 +0300

    Add animation and transition

--- src/templates/imagePage.tsx
+++ src/templates/imagePage.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { motion } from "framer-motion"
+import { motion, Variants } from "framer-motion"
 
 // components
 import { Layout } from "../components"
@@ -10,8 +10,22 @@ import { DataType } from "../components/gallery/galleryContent"
 // styled components
 import { ImgPageContent, ImgWrapper } from "../styles/"
 
+// utils
+import { defaultTransition } from "../utils"
+
 type ImageProps = {
   pageContext: DataType
+}
+
+const variants: Variants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
 }
 
 const ImagePage = ({ pageContext }: ImageProps) => {
@@ -19,7 +33,13 @@ const ImagePage = ({ pageContext }: ImageProps) => {
     <Layout>
       <ImgPageContent>
         <ImgWrapper>
-          <motion.img src={pageContext.cover} />
+          <motion.img
+            src={pageContext.cover}
+            variants={variants}
+            initial={"initial"}
+            animate={"animate"}
+            transition={defaultTransition}
+          />
         </ImgWrapper>
       </ImgPageContent>
     </Layout>
