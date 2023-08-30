cb003bdc7f7369d0ddc890965bccd4f418cb5deb
shaqdeff <shaquillendunda@gmail.com>
2023-08-30 04:35:18 +0300

    Import Layout.jsx component

--- src/templates/imagePage.tsx
+++ src/templates/imagePage.tsx
@@ -1,7 +1,29 @@
 import React from "react"
+import { motion } from "framer-motion"
 
-const ImagePage = () => {
-  return <div>ImagePage</div>
+// components
+import { Layout } from "../components"
+
+// props
+import { DataType } from "../components/gallery/galleryContent"
+
+// styled components
+import { ImgPageContent, ImgWrapper } from "../styles/"
+
+type ImageProps = {
+  pageContext: DataType
+}
+
+const ImagePage = ({ pageContext }: ImageProps) => {
+  return (
+    <Layout>
+      <ImgPageContent>
+        <ImgWrapper>
+          <motion.img src={pageContext.cover} />
+        </ImgWrapper>
+      </ImgPageContent>
+    </Layout>
+  )
 }
 
 export default ImagePage
