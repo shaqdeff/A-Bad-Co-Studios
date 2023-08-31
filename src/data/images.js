97820d779af694208a63222ae4d06f6f687daf2b
shaqdeff <shaquillendunda@gmail.com>
2023-08-31 03:56:51 +0300

    Update data

--- src/data/images.js
+++ src/data/images.js
@@ -1,17 +1,17 @@
 module.exports = [
   {
     cover:
-      "https://images.unsplash.com/photo-1542897643-cfccd88c7127?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1227&q=80",
-    title: "Penglipuran",
-    slug: "penglipuran",
-    color: "#edede9",
-  },
-  {
-    cover:
       "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
     title: "Bbq Kebabs",
     slug: "bbq-kebabs",
     color: "#520001",
+  },
+  {
+    cover:
+      "https://images.unsplash.com/photo-1542897643-cfccd88c7127?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1227&q=80",
+    title: "Penglipuran",
+    slug: "penglipuran",
+    color: "#edede9",
   },
   {
     cover:
