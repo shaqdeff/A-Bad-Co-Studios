8cd9cda8bad0696a5449ad5d6df4a2ea7d09feda
shaqdeff <shaquillendunda@gmail.com>
2023-09-07 12:09:30 +0300

    Remove onRouteChange

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,11 +3,6 @@ import React from "react"
 
 import { GlobalProvider } from "./src/context/globalContext"
 
-export const onRouteUpdate = ({ location, prevLocation }) => {
-  console.log("new pathname", location.pathname)
-  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
-}
-
 export const wrapRootElement = ({ element }) => (
   <GlobalProvider>{element}</GlobalProvider>
 )
