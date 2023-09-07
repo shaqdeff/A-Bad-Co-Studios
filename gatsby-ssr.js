8192e58cff324027e7fab7a9b115c96bdeef6865
shaqdeff <shaquillendunda@gmail.com>
2023-09-07 11:53:13 +0300

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,13 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
- */
+import React from "react"
+// import { Layout } from "./src/components"
 
-/**
- * @type {import('gatsby').GatsbySSR['onRenderBody']}
- */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
+import { GlobalProvider } from "./src/context/globalContext"
+
+export const onRouteUpdate = ({ location, prevLocation }) => {
+  console.log("new pathname", location.pathname)
+  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
 }
+
+export const wrapRootElement = ({ element }) => (
+  <GlobalProvider>{element}</GlobalProvider>
+)
