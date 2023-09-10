b902fd523c4468e530ec32093e8e75b2294dc1aa
shaqdeff <shaquillendunda@gmail.com>
2023-09-10 06:53:44 +0300

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,7 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
+import React from "react"
 
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
+import { GlobalProvider } from "./src/context/globalContext"
+
+export const wrapRootElement = ({ element }) => (
+  <GlobalProvider>{element}</GlobalProvider>
+)
