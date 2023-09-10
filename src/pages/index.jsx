3e4a8ef4b5d6b7f464a60939fba073197b937e3a
shaqdeff <shaquillendunda@gmail.com>
2023-09-10 17:15:13 +0300

    UPdate index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,44 +1,36 @@
-import React, { useEffect } from "react"
-import { useAnimation } from "framer-motion"
+import React, { useEffect, useState } from "react"
 
 // components
-import { Layout, HomeBanner, Loader } from "../components"
+import { Layout, HomeBanner } from "../components"
 
 // context
 import { useGlobalStateContext, useGlobalDispatchContext } from "../context"
 
-// utils
-import { defaultTransition } from "../utils"
-
 const IndexPage = props => {
   const { cursorStyles } = useGlobalStateContext()
   const dispatch = useGlobalDispatchContext()
+
+  const [isClient, setClient] = useState(false)
+  const key = isClient ? "client" : "server"
 
   const onCursor = cursorType => {
     cursorType = (cursorStyles.includes(cursorType) && cursorType) || false
     dispatch({ type: "CURSOR_TYPE", cursorType: cursorType })
   }
 
-  const control = useAnimation()
-
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      control.start({
-        opacity: 0,
-        transition: { defaultTransition },
-      })
-    }, 2500)
+    setClient(true)
+  }, [])
 
-    return () => {
-      clearTimeout(timeoutId)
-      control.stop()
-    }
-  })
+  if (!isClient) {
+    return null
+  }
 
   return (
     <Layout>
-      <Loader loaderControls={control} />
-      <HomeBanner onCursor={onCursor} />
+      <div key={key}>
+        <HomeBanner onCursor={onCursor} />
+      </div>
     </Layout>
   )
 }
