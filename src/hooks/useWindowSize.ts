1322ac004ac78f37b44d27c2891e0cfa2c3dc9a1
shaqdeff <shaquillendunda@gmail.com>
2023-09-11 07:11:45 +0300

    Update useWindowSize.ts

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,22 +1,30 @@
 import { useState, useEffect } from "react"
 
 export default function useWindowSize() {
+  const hasWindow = typeof window !== "undefined"
+
   function getSize() {
+    const width = hasWindow ? window.innerWidth : null
+    const height = hasWindow ? window.innerHeight : null
     return {
-      width: window.innerWidth,
-      height: window.innerHeight,
+      width,
+      height,
     }
   }
 
-  const [windowSize, setWindowSize] = useState(getSize)
-  useEffect(() => {
-    function handleResize() {
-      setWindowSize(getSize())
-    }
+  const [windowSize, setWindowSize] = useState(getSize())
 
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
-  }, [])
+  useEffect(() => {
+    if (hasWindow) {
+      function handleResize() {
+        setWindowSize(getSize())
+      }
+
+      window.addEventListener("resize", handleResize)
+
+      return () => window.removeEventListener("resize", handleResize)
+    }
+  }, [hasWindow])
 
   return windowSize
 }
