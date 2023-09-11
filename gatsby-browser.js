8f6f732758b0852ac95647ddc5e66987f3b494ba
shaqdeff <shaquillendunda@gmail.com>
2023-09-11 07:30:13 +0300

    Update gatsby-browser.js & gatsby-ssr.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,6 @@
 import React from "react"
-// import { Layout } from "./src/components"
 
 import { GlobalProvider } from "./src/context/globalContext"
-
-export const onRouteUpdate = ({ location, prevLocation }) => {
-  console.log("new pathname", location.pathname)
-  console.log("old pathname", prevLocation ? prevLocation.pathname : null)
-}
 
 export const wrapRootElement = ({ element }) => (
   <GlobalProvider>{element}</GlobalProvider>
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,7 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
- */
+import React from "react"
 
-/**
- * @type {import('gatsby').GatsbySSR['onRenderBody']}
- */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
-}
+import { GlobalProvider } from "./src/context/globalContext"
+
+export const wrapRootElement = ({ element }) => (
+  <GlobalProvider>{element}</GlobalProvider>
+)
