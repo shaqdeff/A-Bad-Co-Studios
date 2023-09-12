9c1e3d4c3b66c3ff8634d169f741c9df7678ee7f
shaqdeff <shaquillendunda@gmail.com>
2023-09-12 14:07:19 +0300

    Add modules

--- src/styles/index.d.ts
+++ src/styles/index.d.ts
@@ -1,5 +1,25 @@
 declare module "../../styles/" {
   const BackButton: any
+  const Content: any
+  const GridContent: any
+  const ListContent: any
+  const ContentDragger: any
+  const CircularShape: any
+  const GridElements: any
+  const ThumbnailWrapper: any
+  const GridItemMedia: any
+  const ViewSwitch: any
 }
 
-export {}
+export {
+  BackButton,
+  Content,
+  GridContent,
+  ListContent,
+  ContentDragger,
+  CircularShape,
+  GridElements,
+  ThumbnailWrapper,
+  GridItemMedia,
+  ViewSwitch,
+}
